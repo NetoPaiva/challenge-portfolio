646c012ba2c7b141a2b109860c859f4e6d62ff35
NetoPaiva <netopaiva@yahoo.com>
2023-04-12 14:29:51 -0300

    commit final: incluir link das páginas e repositórios dos projetos; .gitignore; anotações; agradecimento; validacampo.

--- assets/js/validacampo.js
+++ assets/js/validacampo.js
@@ -1,19 +1,22 @@
-// Estrutura de encapsulamento IIFE
-// ( () => { } ) ();
 
-  // receber todos os campos do form
+// receber todos os campos do form
+
   const cadaCampo = document.querySelectorAll('[data-form]');
   // console.log(cadaCampo);
 
+
 // detectar qual campo está em foco
+
   cadaCampo.forEach(campo => {
     campo.addEventListener('blur', evento => {
       validaCampo(evento.target);
     });
   });
 
+
 // função para validar campos
-  function validaCampo(campo) {
+
+  export function validaCampo(campo) {
     if(campo.validity.valid){
       campo.parentElement.classList.remove('input__container--invalido');
     } else {
